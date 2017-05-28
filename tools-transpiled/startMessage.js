7aae68724dde0159bb172190c364ec4df200bc23
arya <arya.adriansyah@gmail.com>
2017-05-28 16:02:41 +0200

    Add transpiled components for node to run.

---
+++ tools-transpiled/startMessage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _colors = require('colors');
+
+var _colors2 = _interopRequireDefault(_colors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('Starting the app in dev-mode'.green); //eslint-disable-line no-console
