e38bb07fd356415f5714a9f6c4cc23ecba205bf7
arya <arya.adriansyah@gmail.com>
2017-06-04 02:56:13 +0200

    Release 1.0 version

--- tools-transpiled/startMessage.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _colors = require('colors');
-
-var _colors2 = _interopRequireDefault(_colors);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log('Starting the app in dev-mode'.green); //eslint-disable-line no-console
